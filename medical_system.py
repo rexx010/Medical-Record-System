c7d41794913b4db5baa4b0cf1424e3407c3cf0a7
Eriife-git <eriifefashonista22@gmail.com>
2025-07-30 18:52:13 +0100

    updated

--- medical_system.py
+++ medical_system.py
@@ -10,49 +10,19 @@ class MedicalSystem:
         self.__doctor_records = {}
         self.__appointments = []
 
+
     def add_patient(self, patient):
-        if patient.get_id in self.__patient_records:
-            raise ValueError("Patient already registered")
-
-        if not patient.name:
-            raise ValueError('Patient name cannot be empty')
-        try:
-            dob = datetime.strptime(patient.dob, '%d/%m/%Y %H:%M')
-        except ValueError:
-            raise ValueError("Date of birth must be in DD/MM/YYYY HH:MM format(eg, 10/05/2000 02:00)")
-
-        if dob > datetime.today():
-            raise ValueError("Date of birth cannot be in the future")
-
-        if not patient.problem:
-            raise ValueError('Patient problem cannot be empty')
-
-        if not patient.speciality_needed:
-            raise ValueError('Speciality cannot be empty')
-
+        self.__validate_patient(patient)
         self.__patient_records[patient.get_id] = patient
 
     def add_doctor(self, new_doctor: Doctor):
-        if new_doctor.get_id in self.__doctor_records:
-            raise ValueError('Doctor already registered')
-
-        if not new_doctor.name:
-            raise ValueError('Doctor name cannot be empty')
-
-        if not new_doctor.specialisation:
-            raise ValueError('Doctor specialisation cannot be empty')
-
+        self.__validate_doctor(new_doctor)
         self.__doctor_records[new_doctor.get_id] = new_doctor
 
 
     def assign_doctor_to_patient(self, patient: Patient, date: str):
-        try:
-            new_date = datetime.strptime(date, '%d/%m/%Y %H:%M')
-        except ValueError:
-            raise ValueError("Appointment date must be in DD/MM/YYYY HH:MM format(eg, 10/05/2000 03:00)")
-
-        if new_date < datetime.today():
-            raise ValueError("Appointment date cannot be in the past")
+        new_date: datetime = self.__validate_appointment_date(date)
+        self.__check_future_date(new_date)
 
         for doctor in self.__doctor_records.values():
             if patient.speciality_needed == doctor.specialisation:
@@ -72,11 +42,23 @@ class MedicalSystem:
             print("No appointments scheduled.")
 
 
+    def view_all_doctors(self):
+        for doctor in self.__doctor_records.values():
+            print(doctor)
+
+        if not self.__doctor_records:
+            print("No doctors available.")
+
+
+    def view_all_patients(self):
+        for patient in self.__patient_records.values():
+            print(patient)
+        if not self.__patient_records:
+            print("No patients available.")
+
+
     def cancel_appointment(self, patient_id: str, date: str):
-        try:
-            cancel_date = datetime.strptime(date, '%d/%m/%Y %H:%M')
-        except ValueError:
-            raise ValueError("Appointment date must be in DD/MM/YYYY HH:MM format")
+        cancel_date: datetime = self.__validate_appointment_date(date)
 
         for appointment in self.__appointments:
             if appointment.patient.get_id == patient_id and appointment.date == cancel_date:
@@ -84,28 +66,18 @@ class MedicalSystem:
                 return f"Appointment for patient {appointment.patient.name} on {date} has been cancelled"
         return "No appointment scheduled for this patient"
 
-    def reschedule_appointment(self, patient_id: str, old_date: str, new_date: str):
-        try:
-            date_old = datetime.strptime(old_date, '%d/%m/%Y %H:%M')
-            date_new = datetime.strptime(new_date, '%d/%m/%Y %H:%M')
-        except ValueError:
-                raise ValueError("New appointment date must be in DD/MM/YYYY HH:MM format")
-        if date_new < datetime.today():
-            raise ValueError("New appointment date cannot be in the past")
+    def reschedule_appointment(self, patient_id: str, old_date_str: str, new_date_str: str):
+        old_date: datetime = self.__validate_appointment_date(old_date_str)
+        new_date: datetime = self.__validate_appointment_date(new_date_str)
+        self.__check_future_date(new_date)
 
         if not patient_id:
             raise ValueError("Patient id cannot be empty")
 
-        if not old_date:
-            raise ValueError("Old date cannot be empty")
-
-        if not new_date:
-            raise ValueError("New date cannot be empty")
-
         for appointment in self.__appointments:
-            if appointment.patient.get_id == patient_id and appointment.date == date_old:
-                appointment.date = date_new
-                return f"Appointment for {appointment.patient.name} has been rescheduled to {new_date}"
+            if appointment.patient.get_id == patient_id and appointment.date == old_date:
+                appointment.date = new_date
+                return f"Appointment for patient {appointment.patient.name} has been rescheduled to {new_date_str}"
         raise ValueError("No appointment scheduled for this patient")
 
 
@@ -129,3 +101,49 @@ class MedicalSystem:
             if ID == doctor_id:
                 return str(doctor)
         raise ValueError("Doctor not found.")
+
+
+    def __validate_patient(self, patient: Patient) -> None:
+        if patient.get_id in self.__patient_records:
+            raise ValueError("Patient already registered")
+
+        if not patient.name:
+            raise ValueError('Patient name cannot be empty')
+        try:
+            dob = datetime.strptime(patient.dob, '%d/%m/%Y')
+        except ValueError:
+            raise ValueError("Date of birth must be in DD/MM/YYYY format(eg, 10/05/2000)")
+
+        if dob > datetime.today():
+            raise ValueError("Date of birth cannot be in the future")
+
+        if not patient.problem:
+            raise ValueError('Patient problem cannot be empty')
+
+        if not patient.speciality_needed:
+            raise ValueError('Speciality cannot be empty')
+
+
+    def __validate_doctor(self, doctor: Doctor) -> None:
+        if doctor.get_id in self.__doctor_records:
+            raise ValueError('Doctor already registered')
+
+        if not doctor.name:
+            raise ValueError('Doctor name cannot be empty')
+
+        if not doctor.specialisation:
+            raise ValueError('Doctor specialisation cannot be empty')
+
+
+    @staticmethod
+    def __validate_appointment_date(date: str) -> datetime:
+        try:
+            return datetime.strptime(date, '%d/%m/%Y %H:%M')
+        except ValueError:
+            raise ValueError("Appointment date must be in DD/MM/YYYY HH:MM format")
+
+    @staticmethod
+    def __check_future_date(appointment_date: datetime):
+        if appointment_date < datetime.today():
+            raise ValueError("New appointment date cannot be in the past")
+
--- test_medical_system.py
+++ test_medical_system.py
@@ -1,22 +1,25 @@
 import unittest
+from datetime import datetime, timedelta
 from medical_system import MedicalSystem
 from patient import Patient
 from doctor import Doctor
+from appointment import Appointment
 
 class TestMedicalSystem(unittest.TestCase):
 
     def setUp(self):
         self.system = MedicalSystem()
         self.new_doc = Doctor("Dr. Will", "Dermatology", {"phone": "123456789", "email": "will@gmail.com", "home address": "2, broad street", "gender": "Male"})
-        self.new_patient = Patient("Ade", "10/10/1995 03:00", "Skin rash", "Dermatology", {"phone": "123456789", "gender": "female", "address": "12 broad way", "email": "ade@gmail.com"})
+        self.new_patient = Patient("Ade", "10/10/1995", "Skin rash", "Dermatology", {"phone": "123456789", "gender": "female", "address": "12 broad way", "email": "ade@gmail.com"})
 
     def test_that_medical_system_can_add_patient(self):
         self.system.add_patient(self.new_patient)
         self.assertEqual(self.system.find_patient_record(self.new_patient.get_id), str(self.new_patient))
 
-    def test_that_medical_system_cannot_add_same_patient_id(self):
+    def test_that_medical_system_cannot_add_same_patient_twice(self):
         self.system.add_patient(self.new_patient)
         self.assertRaises(ValueError, self.system.add_patient, self.new_patient)
+
 
     def test_that_medical_system_cannot_add_patient_with_future_dob(self):
         wrong_patient = Patient("ada", "01/01/2026", "Skin disease", "Dermatology", {"phone": "123456789", "gender": "female", "address": "12 broad lane", "email": "ada@gamil.com"})
@@ -60,41 +63,68 @@ class TestMedicalSystem(unittest.TestCase):
     def test_that_medical_system_can_assign_doctor_to_patient(self):
         self.system.add_patient(self.new_patient)
         self.system.add_doctor(self.new_doc)
-        result = self.system.assign_doctor_to_patient(self.new_patient, "25/08/2025 03:00")
+        result = self.system.assign_doctor_to_patient(self.new_patient, "25/08/2026 03:00")
         self.assertIn("Appointment has been scheduled for patient Ade", result)
 
 
     def test_that_medical_system_cannot_find_doctor(self):
-        another_patient = Patient('Ice', '10/01/1995 02:00', 'toothache', 'Dentistry', {'phone number': '0000000000', 'email': 'ice@email.com', 'home address': 'Den close'})
+        another_patient = Patient('Ice', '10/01/1995', 'toothache', 'Dentistry', {'phone number': '123456789', 'email': 'ice@email.com', 'home address': 'Den close'})
         self.system.add_patient(another_patient)
         self.system.add_doctor(self.new_doc)
-        result = self.system.assign_doctor_to_patient(another_patient, "25/08/2025 02:00")
+        result = self.system.assign_doctor_to_patient(another_patient, "25/08/2026 02:00")
         self.assertIn("No doctor available for this patient", result)
 
 
+    def test_can_create_appointment_with_datetime_object(self):
+        future_datetime = datetime(2025, 12, 25, 14, 30)
+        appointment = Appointment(self.new_patient, self.new_doc, future_datetime)
+        self.assertEqual(appointment.date, future_datetime)
+
+
+    def test_appointment_raises_error_for_invalid_date_type(self):
+        self.assertRaises(ValueError, Appointment, self.new_patient, self.new_doc, 12345)
+
+
+    def test_appointment_raises_error_for_past_date(self):
+        past_date = "01/01/2020 09:00"
+        self.assertRaises(ValueError, Appointment, self.new_patient, self.new_doc, past_date)
+
+
+    def test_appointment_raises_error_for_invalid_string_format(self):
+        invalid_format = "2025/12/26 02:30"
+        self.assertRaises(ValueError, Appointment, self.new_patient, self.new_doc, invalid_format)
+
+    def test_appointment_str_method(self):
+        future_date = datetime(2025, 12, 25, 14, 30)
+        appointment = Appointment(self.new_patient, self.new_doc, future_date)
+        expected = (f"Appointment:\n"
+                    f"Patient: {self.new_patient.name} with ID: {self.new_patient.get_id}\n"
+                    f"Doctor: {self.new_doc.name} with ID: {self.new_doc.get_id}\n"
+                    f"Date: {future_date.strftime('%d/%m/%Y %H:%M')}")
+        self.assertEqual(str(appointment), expected)
 
     def test_that_medical_system_can_cancel_appointment(self):
         self.system.add_patient(self.new_patient)
         self.system.add_doctor(self.new_doc)
-        appointment_date = "25/08/2025 03:00"
+        appointment_date = "25/08/2026 03:00"
         self.system.assign_doctor_to_patient(self.new_patient, appointment_date)
 
         cancel_result = self.system.cancel_appointment(self.new_patient.get_id, appointment_date)
-        self.assertIn("Appointment for patient Ade on 25/08/2025 03:00 has been cancelled", cancel_result)
+        self.assertIn("Appointment for patient Ade on 25/08/2026 03:00 has been cancelled", cancel_result)
 
         result = self.system.get_appointment(self.new_patient.get_id)
         self.assertEqual(result, "No appointments found for this patient")
 
     def test_that_medical_system_cannot_cancel_appointment_without_booking(self):
-        result = self.system.cancel_appointment("", "25/08/2025 03:00")
+        result = self.system.cancel_appointment("", "25/08/2026 03:00")
         self.assertIn("No appointment scheduled for this patient", result)
 
     def test_that_medical_system_can_reschedule_appointment(self):
         self.system.add_patient(self.new_patient)
         self.system.add_doctor(self.new_doc)
-        self.system.assign_doctor_to_patient(self.new_patient, '27/07/2025 14:00')
-        result = self.system.reschedule_appointment(self.new_patient.get_id, '27/07/2025 14:00','30/07/2025 11:30')
-        self.assertIn("has been rescheduled", result)
+        self.system.assign_doctor_to_patient(self.new_patient, "27/09/2026 14:00")
+        result = self.system.reschedule_appointment(self.new_patient.get_id, "27/09/2026 14:00","30/09/2026 11:30")
+        self.assertIn("Appointment for patient Ade has been rescheduled to 30/09/2026 11:30", result)
 
     def test_that_medical_system_cannot_reschedule_appointment(self):
         self.assertRaises(ValueError, self.system.reschedule_appointment, "", "01/01/2024 10:00", "02/01/2024 11:00")
@@ -107,7 +137,8 @@ class TestMedicalSystem(unittest.TestCase):
     def test_that_medical_record_can_get_appointment(self):
         self.system.add_patient(self.new_patient)
         self.system.add_doctor(self.new_doc)
-        self.system.assign_doctor_to_patient(self.new_patient, '27/07/2025 14:00')
+        date: str = (datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y %H:%M")
+        self.system.assign_doctor_to_patient(self.new_patient, date)
         result = self.system.get_appointment(self.new_patient.get_id)
         self.assertIn("Appointment", result)
 
